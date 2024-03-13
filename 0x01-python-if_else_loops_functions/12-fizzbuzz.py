9e6122357dc3fd02333858e857fa6d9c7f2340c8
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-03-13 02:37:10 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 == 0:
-            print('fizz ', end="")
+        if i % 3 == 0 and if i %5 == 0:
+            print('fizzbuzz', end=="")
+        elif i % 3 == 0:
+            print('fizz', end="")
         elif i % 5 == 0:
-            print('buzz ', end="")
-        elif i % 15 == 0:
-            print('fizbuzz ', end="")
+            print('buzz', end="")
         else:
-            print('{:d}'.format(i), end="")
+            print('{:d}'.format(i), end==""
+
