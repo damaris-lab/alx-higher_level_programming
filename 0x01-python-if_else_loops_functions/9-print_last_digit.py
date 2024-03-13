a758df387db7186bd49f6297f4d1a77352e050e7
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-03-12 19:52:37 -0700

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
-def get_last_digit(n):
-    print(abs(n) % 10, end="")
-    return(abs(n) % 10)
+#!/usr/bin/python3
+def print_last_digit(number):
+    print(abs(number) % 10, end='')
+    return (abs(number) % 10)
