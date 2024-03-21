135792ce88e8cc12f8645b0d0b20a52ca31c10dc
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-03-20 18:22:00 -0700

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not dictionary:
+        return none
+    lst = list(a_dictionary[lst])
+
+    max = a_dictionary[0]
+    for lst in lst:
+        if a_dictionary[key] > max:
+            max = a_dictionary[key]
+            reslt = key
+    return (rslt)
