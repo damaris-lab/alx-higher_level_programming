9ca8b1d3c5b5dca614e056c0630fff5a3e2ed04a
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-03-20 18:24:33 -0700

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,10 @@
 def best_score(a_dictionary):
     if not dictionary:
         return none
-    lst = list(a_dictionary[lst])
+    lst = list(a_dictionary)
 
-    max = a_dictionary[0]
-    for lst in lst:
+    max = a_dictionary[lst[0]]
+    for key in lst:
         if a_dictionary[key] > max:
             max = a_dictionary[key]
             reslt = key
