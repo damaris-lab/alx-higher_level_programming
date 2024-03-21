a55ebb31f4eecd45aaef04a23c15536095483b02
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-03-20 17:50:39 -0700

    6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    lst = list(a_dictionary)
+    lst.sort()
+
+    for item in lst:
+        print("{}:{}".format(item, a_dictionary))
