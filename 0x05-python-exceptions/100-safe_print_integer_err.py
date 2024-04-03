cb801824ce0f21c2c57582c7d682ceb26a01c196
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-04-02 18:46:17 -0700

    100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError) as V:
+        sys.stderr.write("Exception: " + V.args[0] + "\n")
+        return False
