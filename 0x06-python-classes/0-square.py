2ee8513c400a57527987b911906fc86c8cad53da
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-04-03 18:44:42 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """module doumentation
-
-This class does not import modules 
-
+class does not import modules
 """
+
+
 class Square:
     """square is empty
     it defines a square
