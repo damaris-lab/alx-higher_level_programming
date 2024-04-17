a3ee4902e8f842425afbd79859737a9ea087ab0f
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-04-17 01:16:12 +0000

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+The student module
+"""
+
+
+class Student:
+    """The student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        to_json - returns a dictionary representation of an
+        instance of class Student
+        """
+        result = {}
+        if attrs is None:
+            return self.__dict__
+        for item in attrs:
+            if hasattr(self, item):
+                result[item] = getattr(self, item)
+        return result
+
+    def reload_from_json(self, json):
+        """reload_from_json: resets all the instance attributes
+        with values from a dictionary
+        """
+
+        for key in json:
+            setattr(self, key, json[key])
