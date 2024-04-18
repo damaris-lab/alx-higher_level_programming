eff38f30ab71b98208e7a9862d7d97d46cc54a0f
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-04-18 02:13:11 +0000

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+BaseGeometry class definition
+"""
+
+
+class BaseGeometry:
+	"""
+	public method
+	"""
+	
+	def area(self):
+		"""raises an exception"""
+		raise Exception("area() is not implemented")
