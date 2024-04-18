0fb100985914fb51acd7cc75bfca3e92b24824aa
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-04-18 03:04:52 +0000

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-
-"""inherits fro function definition
+"""
+inherits from function definition
 """
 
 
 def inherits_from(obj, a_class):
-	
-	"""
-	function returns True:object is class instance that
-	inherited from pecified class(directly or indirectly)
-	else:False
-	"""
-	
-	return issubclass(type(obj), a_class) and type(obj) != a_class
+
+    """
+    function returns True: object is class instance that
+    inheriteed from specified class(directly or indirectly)
+    else: False
+    """
+
+    return issubclass(type(obj), a_class) and type(obj) != a_class
