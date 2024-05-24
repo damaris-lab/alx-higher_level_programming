ea9e43871bf3722b086204a930709429fd2d07c5
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-05-23 19:37:01 -0700

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,13 +1,7 @@
---  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT subquery.id, subquery.title, tv_show_genres.genre_id
-FROM (
-    SELECT tv_shows.id, tv_shows.title
-    FROM tv_shows
-    WHERE tv_shows.id IN (
-        SELECT tv_show_genres.tv_show_id
-        FROM tv_show_genres
-    )
-) AS subquery
-JOIN tv_show_genres ON subquery.id = tv_show_genres.tv_show_id
-ORDER BY subquery.title ASC, tv_show_genres.genre_id ASC;
+--  lists all shows contained in hbtn_0d_tvshows that have at least one genre
+SELECT tv_shows.title AS title, tv_show-genres.genre_id AS genre_id
+FROM tv_show_genres
+INNER JOIN tv_shows
+ON tv_show_genres.sgow_id = tv_shows.id
+ORDER BY title ASC, genre_id ASC,
 
