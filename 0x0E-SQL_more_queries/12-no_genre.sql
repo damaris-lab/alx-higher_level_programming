6eb915c22f9e39dba1343c7343b6785c7154b47d
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-05-23 19:50:03 -0700

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,5 +3,5 @@ SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_show_genres
 RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_show_genres.show_id IS NULL
-ORDER BY title ASC, genre_id ASC:
+ORDER BY title ASC, genre_id ASC;
 
