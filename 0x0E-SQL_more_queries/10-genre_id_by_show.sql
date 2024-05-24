d42479e34e89ce632e3ea3ab40705989c133e401
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-05-23 19:41:22 -0700

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,5 +3,5 @@ SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_show_genres
 INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-ORDER BY title ASC, genre_id ASC,
+ORDER BY title ASC, genre_id ASC;
 
