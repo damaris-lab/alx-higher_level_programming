d39d7e6c46a625c2e8560995a9ad96f4669e9526
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-05-23 19:48:56 -0700

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,14 +1,7 @@
 --  lists all shows contained in hbtn_0d_tvshows without a genre linked
-SELECT subquery.id, subquery.title, tv_show_genres.genre_id
-FROM (
-    SELECT tv_shows.id, tv_shows.title
-    FROM tv_shows
-    WHERE tv_shows.id NOT IN (
-        SELECT tv_show_genres.tv_show_id
-        FROM tv_show_genres
-    )
-) AS subquery
-LEFT JOIN tv_show_genres ON subquery.id = tv_show_genres.tv_show_id
-WHERE tv_show_genres.tv_show_id IS NULL
-ORDER BY subquery.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_show_genres
+RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title ASC, genre_id ASC:
 
