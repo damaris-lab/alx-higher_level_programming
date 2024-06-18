30cabf432257bc97e170a848d08f207e2357a815
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-06-18 23:27:55 +0000

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let rowStr;
+    for (let i = 0; i < this.height; i++) {
+      rowStr = '';
+      for (let j = 0; j < this.width; j++) {
+        rowStr += 'X';
+      }
+      console.log(rowStr);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
