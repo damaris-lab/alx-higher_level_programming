c26ce97fd2f58449be28993779e709f21e743479
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-06-20 01:33:35 +0000

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-if (isNaN(num)) {
-    console.log("Not a number");
-  } else {
-    console.log(`My number: ${num}`);
-  }
+#!/usr/bin/node
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', parseInt(process.argv[2]));
+}
